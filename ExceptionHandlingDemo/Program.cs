4cb2204b7d84c8ddab7946a6d18e52c32b80c9f6
srinivasthoudoju <sthoudoju@verisk.com>
2024-10-05 13:02:41 +0530

    Csharp Project is pushing

---
+++ ExceptionHandlingDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(args[0]);
+            int n2 = int.Parse(args[1]);
+
+            int result = n1 / n2;
+            Console.WriteLine("The value of result is = " + result);
+        }
+    }
+}
